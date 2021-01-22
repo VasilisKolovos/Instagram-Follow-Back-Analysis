822637b540c300ef1fdc31ec7a01a8a00829035c
DESKTOP-3DKJ2D6\vasil <vasiliskolovos6@gmail.com>
2021-01-22 10:27:56 +0200

    Upload of .exe file and update on Read me. Also some reshape of the folder

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Instagram-Follow-Back-Analysis
 A program that founds the accounts that doesn't follow you back.
 Please download the two .csv files in order for this to work.
-There is no .exe file provided so you have to run the code natively.
+You can use this link to download the tool needed in order to get the files.
+( https://chrome.google.com/webstore/detail/helper-tools-for-instagra/clibiflfecckdjnjcgcgjdknmbgceail )
+Please remember that the files must be .csv format.
--- main.cpp
+++ main.cpp
@@ -4,42 +4,42 @@
 #include <string>
 using namespace std;
 
-void whatif () {
-    ifstream file1,file2;
+void names() {
+    ifstream file1, file2;
     file1.open("followers.csv");
     file2.open("following.csv");
     if (file1.is_open()) {
         if (file2.is_open()) {
             string line1;
             string line2;
-            getline(file1,line1);
-            getline(file2,line2);
-            int m1=0,m2=0;
-            while (getline(file1,line1)) {
+            getline(file1, line1);
+            getline(file2, line2);
+            int m1 = 0, m2 = 0;
+            while (getline(file1, line1)) {
                 m1++;
             }
-            while (getline(file2,line2)) {
+            while (getline(file2, line2)) {
                 m2++;
             }
             file1.close();
             file2.close();
             file1.open("followers.csv");
             file2.open("following.csv");
-            getline(file1,line1);
-            getline(file2,line2);
-            for (int i=0; i<=m2; i++) {
-                getline(file2,line2,',');
-                getline(file2,line2);
-                for (int j=0; j<=m1; j++) {
-                    getline(file1,line1,',');
-                    getline(file1,line1);
-                    if (line2==line1) {
+            getline(file1, line1);
+            getline(file2, line2);
+            for (int i = 0; i <= m2; i++) {
+                getline(file2, line2, ',');
+                getline(file2, line2);
+                for (int j = 0; j <= m1; j++) {
+                    getline(file1, line1, ',');
+                    getline(file1, line1);
+                    if (line2 == line1) {
                         file1.close();
                         break;
                     }
                     else {
-                        if (j==m1) {
-                            cout<<line2<<endl;
+                        if (j == m1) {
+                            cout << line2 << endl;
                             file1.close();
                         }
                     }
@@ -47,12 +47,19 @@ void whatif () {
                 file1.open("followers.csv");
             }
         }
+        else {
+            cout << "One or more files were not able to open" << endl;
+        }
+    }
+    else {
+        cout << "One or more files were not able to open" << endl;
     }
     file1.close();
     file2.close();
 }
 
 int main(int argc, char** argv) {
-    whatif();    
+    names();
+    system("pause");
     return 0;
 }
